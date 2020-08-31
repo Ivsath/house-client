17227d884a540b8c109f0c53123fe33d2086bf71
Irvin Reyes <reyes.yerp@gmail.com>
2020-08-30 22:28:18 -0500

    Create our application routes

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,34 @@ import ApolloClient from "apollo-boost";
 import React from "react";
 import { ApolloProvider } from "react-apollo";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { Listings } from "./sections";
+import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
 import * as serviceWorker from "./serviceWorker";
 
 const client = new ApolloClient({
   uri: "/api",
 });
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Listings title="Listings Title" />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root"),
