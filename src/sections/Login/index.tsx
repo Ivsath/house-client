76d46d58d5a09966340961ec01a0dd30395cec32
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-11 10:54:01 -0500

    Create the UI for login

--- src/sections/Login/index.tsx
+++ src/sections/Login/index.tsx
@@ -1,5 +1,41 @@
+import { Card, Layout, Typography } from "antd";
 import React from "react";
 
+import googleLogo from "./assets/google_logo.jpg";
+
+const { Content } = Layout;
+const { Text, Title } = Typography;
+
 export const Login = () => {
-  return <h2>Login</h2>;
+  return (
+    <Content className="log-in">
+      <Card className="log-in-card">
+        <div className="log-in-card__intro">
+          <Title level={3} className="log-in-card__intro-title">
+            <span role="img" aria-label="wave">
+              👋
+            </span>
+          </Title>
+          <Title level={3} className="log-in-card__intro-title">
+            Log in to TinyHouse!
+          </Title>
+          <Text>Sign in with Google to start booking available rentals!</Text>
+        </div>
+        <button className="log-in-card__google-button">
+          <img
+            src={googleLogo}
+            alt="Google Logo"
+            className="log-in-card__google-button-logo"
+          />
+          <span className="log-in-card__google-button-text">
+            Sign in with Google
+          </span>
+        </button>
+        <Text type="secondary">
+          Note: By signing in, you&apos;ll be redirected to the Google consent
+          form to sign in with your Google account.
+        </Text>
+      </Card>
+    </Content>
+  );
 };
