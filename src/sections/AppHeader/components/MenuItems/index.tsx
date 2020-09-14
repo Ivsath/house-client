bfba291e0ce99b2ac519e8a185bcf2090c9aa86a
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-14 11:10:35 -0500

    Create the MenuItems component

---
+++ src/sections/AppHeader/components/MenuItems/index.tsx
@@ -0,0 +1,24 @@
+import { HomeOutlined } from "@ant-design/icons";
+import { Button, Menu } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const { Item, SubMenu } = Menu;
+
+export const MenuItems = () => {
+  return (
+    <Menu mode="horizontal" selectable={false} className="menu">
+      <Item key="/host">
+        <Link to="/host">
+          <HomeOutlined />
+          Host
+        </Link>
+      </Item>
+      <Item key="/login">
+        <Link to="/login">
+          <Button type="primary">Sign In</Button>
+        </Link>
+      </Item>
+    </Menu>
+  );
+};
