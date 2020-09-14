ab432890806f93b982b9031c53b472c8124e8e66
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-13 22:52:22 -0500

    Fix title

--- src/sections/Login/index.tsx
+++ src/sections/Login/index.tsx
@@ -92,7 +92,7 @@ export const Login = ({ setViewer }: Props) => {
             </span>
           </Title>
           <Title level={3} className="log-in-card__intro-title">
-            Log in to TinyHouse!
+            Log in to House!
           </Title>
           <Text>Sign in with Google to start booking available rentals!</Text>
         </div>
