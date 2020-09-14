944f2caa9007bd7f21c938d79fe7ccba23a0388e
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-14 13:01:33 -0500

    Implement the logout functionality

--- src/sections/AppHeader/components/MenuItems/index.tsx
+++ src/sections/AppHeader/components/MenuItems/index.tsx
@@ -1,27 +1,61 @@
 import { HomeOutlined, LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { useMutation } from "@apollo/react-hooks";
 import { Avatar, Button, Menu } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { LOG_OUT } from "../../../../lib/graphql/mutations";
+import { LogOut as LogOutData } from "../../../../lib/graphql/mutations/LogOut/__generated__/LogOut";
 import { Viewer } from "../../../../lib/types";
+import {
+  displayErrorMessage,
+  displaySuccessNotification,
+} from "../../../../lib/utils";
 
 interface Props {
   viewer: Viewer;
+  setViewer: (viewer: Viewer) => void;
 }
 
 const { Item, SubMenu } = Menu;
 
-export const MenuItems = ({ viewer }: Props) => {
+export const MenuItems = ({ viewer, setViewer }: Props) => {
+  const [logOut] = useMutation<LogOutData>(LOG_OUT, {
+    onCompleted: (data) => {
+      if (data && data.logOut) {
+        setViewer(data.logOut);
+        displaySuccessNotification("You've successfully logged out!");
+      }
+    },
+    onError: (_data) => {
+      displayErrorMessage(
+        "Sorry! We weren't able to log you out. Please try again later!",
+      );
+    },
+  });
+
+  const handleLogOut = () => {
+    logOut();
+  };
+
   const subMenuLogin =
     viewer.id && viewer.avatar ? (
       <SubMenu title={<Avatar src={viewer.avatar} />}>
         <Item key={"/user/"}>
-          <UserOutlined />
-          Profile
+          <Link to={`/user/${viewer.id}`}>
+            <UserOutlined />
+            Profile
+          </Link>
         </Item>
         <Item key="logout">
-          <LogoutOutlined />
-          Log out
+          <div
+            onClick={handleLogOut}
+            onKeyPress={handleLogOut}
+            role="button"
+            tabIndex={0}>
+            <LogoutOutlined />
+            Log out
+          </div>
         </Item>
       </SubMenu>
     ) : (
