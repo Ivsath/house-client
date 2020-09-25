246eb90c121492d212e96e5441832c9916afded3
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-24 20:06:43 -0500

    Make query on the user component

--- src/sections/User/index.tsx
+++ src/sections/User/index.tsx
@@ -1,5 +1,27 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
 
-export const User = () => {
-  return <h2>User</h2>;
+import { USER } from "../../lib/graphql/queries";
+import {
+  User as UserData,
+  UserVariables,
+} from "../../lib/graphql/queries/User/__generated__/User";
+
+interface MatchParams {
+  id: string;
+}
+
+export const User = ({ match }: RouteComponentProps<MatchParams>) => {
+  const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
+    variables: {
+      id: match.params.id,
+    },
+  });
+
+  return (
+    <div>
+      <h2>User</h2>
+    </div>
+  );
 };
