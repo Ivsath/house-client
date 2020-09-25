5ab82ed743d8193383e04bc8c343a035b061a6af
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-24 21:23:32 -0500

    Show the user profile on the user component

--- src/sections/User/index.tsx
+++ src/sections/User/index.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@apollo/client";
+import { Col, Layout, Row } from "antd";
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
 
@@ -7,10 +8,13 @@ import {
   User as UserData,
   UserVariables,
 } from "../../lib/graphql/queries/User/__generated__/User";
+import { UserProfile } from "./components";
 
 interface MatchParams {
   id: string;
 }
+
+const { Content } = Layout;
 
 export const User = ({ match }: RouteComponentProps<MatchParams>) => {
   const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
@@ -19,9 +23,14 @@ export const User = ({ match }: RouteComponentProps<MatchParams>) => {
     },
   });
 
+  const user = data ? data.user : null;
+  const userProfileElement = user ? <UserProfile user={user} /> : null;
+
   return (
-    <div>
-      <h2>User</h2>
-    </div>
+    <Content className="user">
+      <Row gutter={12} justify="space-between">
+        <Col xs={24}>{userProfileElement}</Col>
+      </Row>
+    </Content>
   );
 };
