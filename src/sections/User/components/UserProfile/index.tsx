18a4c79db7460fdf49f98fa064bc8f2308cfe07e
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-05 11:01:12 -0500

    Add redirect to stripe

--- src/sections/User/components/UserProfile/index.tsx
+++ src/sections/User/components/UserProfile/index.tsx
@@ -8,9 +8,14 @@ interface Props {
   viewerIsUser: boolean;
 }
 
+const stripeAuthUrl = `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_S_CLIENT_ID}&scope=read_write`;
 const { Paragraph, Text, Title } = Typography;
 
 export const UserProfile = ({ user, viewerIsUser }: Props) => {
+  const redirectToStripe = () => {
+    window.location.href = stripeAuthUrl;
+  };
+
   const additionalDetailsSection = viewerIsUser ? (
     <>
       <Divider />
@@ -20,7 +25,10 @@ export const UserProfile = ({ user, viewerIsUser }: Props) => {
           Interested in becoming a House host? Register with your Stripe
           account!
         </Paragraph>
-        <Button type="primary" className="user-profile__details-cta">
+        <Button
+          type="primary"
+          className="user-profile__details-cta"
+          onClick={redirectToStripe}>
           Connect with Stripe!
         </Button>
         <Paragraph type="secondary">
