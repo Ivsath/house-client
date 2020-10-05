1ac9993f82c77d49ba1f592fe12742711144a78e
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-05 13:09:12 -0500

    Implement the disconnect stripe mutation

--- src/sections/User/components/UserProfile/index.tsx
+++ src/sections/User/components/UserProfile/index.tsx
@@ -1,19 +1,54 @@
+import { useMutation } from "@apollo/client";
 import { Avatar, Button, Card, Divider, Tag, Typography } from "antd";
 import React from "react";
 
+import { DISCONNECT_STRIPE } from "../../../../lib/graphql/mutations/";
+import { DisconnectStripe as DisconnectStripeData } from "../../../../lib/graphql/mutations/DisconnectStripe/__generated__/DisconnectStripe";
 import { User as UserData } from "../../../../lib/graphql/queries/User/__generated__/User";
-import { formatListingPrice } from "../../../../lib/utils";
+import { Viewer } from "../../../../lib/types";
+import {
+  displayErrorMessage,
+  displaySuccessNotification,
+  formatListingPrice,
+} from "../../../../lib/utils";
 
 interface Props {
   user: UserData["user"];
+  viewer: Viewer;
   viewerIsUser: boolean;
+  setViewer: (viewer: Viewer) => void;
 }
 
 // In the stripe platform settings, add a redirect URI
 const stripeAuthUrl = `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_S_CLIENT_ID}&scope=read_write`;
 const { Paragraph, Text, Title } = Typography;
 
-export const UserProfile = ({ user, viewerIsUser }: Props) => {
+export const UserProfile = ({
+  user,
+  viewer,
+  viewerIsUser,
+  setViewer,
+}: Props) => {
+  const [disconnectStripe, { loading }] = useMutation<DisconnectStripeData>(
+    DISCONNECT_STRIPE,
+    {
+      onCompleted: (data) => {
+        if (data && data.disconnectStripe) {
+          setViewer({ ...viewer, hasWallet: data.disconnectStripe.hasWallet });
+          displaySuccessNotification(
+            "You've successfully disconnected from Stripe!",
+            "You'll have to reconnect with Stripe to continue to create listings.",
+          );
+        }
+      },
+      onError: () => {
+        displayErrorMessage(
+          "Sorry! We weren't able to disconnect you from Stripe. Please try again later!",
+        );
+      },
+    },
+  );
+
   const redirectToStripe = () => {
     window.location.href = stripeAuthUrl;
   };
@@ -29,7 +64,12 @@ export const UserProfile = ({ user, viewerIsUser }: Props) => {
           {user.income ? formatListingPrice(user.income) : `$0`}
         </Text>
       </Paragraph>
-      <Button type="primary" danger className="user-profile__details-cta">
+      <Button
+        type="primary"
+        danger
+        className="user-profile__details-cta"
+        loading={loading}
+        onClick={() => disconnectStripe()}>
         Disconnect Stripe
       </Button>
       <Paragraph type="secondary">
