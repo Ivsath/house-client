2363bf4ce5ca91650f02cbbaa4e9bf9685bb2528
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-12 15:46:24 -0500

    Fix DatePicker to use with jest

--- src/lib/components/DatePicker/index.ts
+++ src/lib/components/DatePicker/index.ts
@@ -1,6 +1,6 @@
-import "antd/es/date-picker/style/index";
+import "antd/lib/date-picker/style/index";
 
-import generatePicker from "antd/es/date-picker/generatePicker";
+import generatePicker from "antd/lib/date-picker/generatePicker";
 import { Dayjs } from "dayjs";
 import dayjsGenerateConfig from "rc-picker/lib/generate/dayjs";
 
